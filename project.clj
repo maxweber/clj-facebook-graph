f916cb50abd2e2f3fddbcec3b1fa934c41ea45cf
Robert Levy <r.p.levy@gmail.com>
2011-12-18 12:14:24 -0500

    update project.clj in preparation for upgrading to clojure 1.3

--- project.clj
+++ project.clj
@@ -1,13 +1,10 @@
-(defproject clj-facebook-graph "0.3.0-SNAPSHOT"
+(defproject rplevy/clj-facebook-graph "0.4.0"
   :description "A Clojure client for the Facebook Graph API."
-  :dependencies [[org.clojure/clojure "1.2.1"]
-                 [org.clojure/clojure-contrib "1.2.0"]
-                 [ring/ring-core "0.3.5"]
-                 [clj-http "0.1.3"]
-                 [clj-oauth2 "0.0.1-SNAPSHOT"]]
-  :dev-dependencies [[swank-clojure "1.2.1"]
-                     [ring/ring-jetty-adapter "0.3.5"]
-                     [ring/ring-devel "0.3.5"]
-                     [ring/ring-jetty-adapter "0.3.5"]
-                     [compojure "0.5.3"]]
+  :dependencies [[org.clojure/clojure "1.3.0"]
+                 [ring/ring-core "1.0.1"]
+                 [clj-http "0.2.6"]
+                 [org.clojars.adamwynne/clj-oauth "1.2.12"]]
+  :dev-dependencies [[ring/ring-devel "1.0.1"]
+                     [ring/ring-jetty-adapter "1.0.0"]
+                     [compojure "0.6.4"]]
   :aot [clj-facebook-graph.FacebookGraphException])
