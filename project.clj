287319b1cbeda9ae1781d5a212faf72ebce3e6af
Robert Levy <r.p.levy@gmail.com>
2011-12-18 14:40:33 -0500

    committing with generic project group and preemptively referring post-pull-request oauth2 dependency

--- project.clj
+++ project.clj
@@ -1,10 +1,10 @@
-(defproject rplevy/clj-facebook-graph "0.4.0"
+(defproject clj-facebook-graph "0.4.0"
   :description "A Clojure client for the Facebook Graph API."
   :dependencies [[org.clojure/clojure "1.3.0"]
                  [org.clojure/data.json "0.1.1"]
                  [ring/ring-core "1.0.1"]
                  [clj-http "0.2.6"]
-                 [org.clojars.adamwynne/clj-oauth "1.2.12"]]
+                 [clj-oauth2 "0.1.0"]]
   :dev-dependencies [[ring/ring-devel "1.0.1"]
                      [ring/ring-jetty-adapter "1.0.0"]
                      [compojure "0.6.4"]]
